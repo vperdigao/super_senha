cf962280eaa5dd5cedc5022242845ef1b44bd3ab
Vinicius Perdigão <git@vini.me>
2025-07-31 11:48:57 -0300

    Update application domain and NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.super_senha"
+    namespace = "me.vini.super_senha"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.super_senha"
+        applicationId = "me.vini.super_senha"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
